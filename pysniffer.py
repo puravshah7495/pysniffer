b07e7baeaccfdb2ad277ddfb68dd99ed02fb03f9
Purav Shah <puravshah23@gmail.com>
2016-10-20 17:26:02 -0400

    Fix functin order error

--- pysniffer.py
+++ pysniffer.py
@@ -1,12 +1,11 @@
 import socket
 
+def parseData(data):
+    #placeholder for now
+    print data.decode('hex')
+
 sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
 while True:
     data, address = sniffer.recvfrom(65565)
     parseData(data)
-
-
-def parseData(data):
-    #placeholder for now
-    print data
