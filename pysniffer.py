5ebd14c908e3487faecf31c02221a5aef7e30c4f
Purav Shah <puravshah23@gmail.com>
2016-10-22 00:24:42 -0400

    Modify code to work on windows

--- pysniffer.py
+++ pysniffer.py
@@ -1,11 +1,19 @@
 import socket
+import platform
 
 def parseData(data):
-    #placeholder for now
+    # #placeholder for now
     print data.decode('hex')
 
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+if platform.system() == "Windows":
+	sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+	sniffer.bind(('localhost', 0))
+	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+else:
+	sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 while True:
-    data, address = sniffer.recvfrom(65565)
-    parseData(data)
+	data, address = sniffer.recvfrom(65565)
+	print data
