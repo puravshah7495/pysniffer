531ab9dbfee3be02952014e05f1f6f4877d7f319
Purav Shah <Purav.Shah@stonybrook.edu>
2016-10-24 13:19:02 -0400

    Input and output to dump file

--- sniffer.py
+++ sniffer.py
@@ -3,46 +3,79 @@ import platform
 import struct
 import datetime
 import binascii
+import cPickle as pickle
 
 class IP:
-    def __init__(self, src, dest, version, length, protocol, ttl):
+    def __init__(self, version, ihl, service, length, id, flags, offset, ttl, prot, checksum, src, dest, payload):
+        self.version = version
+        self.ihl = ihl
+        self.service = service
+        self.length = length
+        self.id = id
+        self.flags = flags
+        self.offset = offset
+        self.ttl = ttl
+        self.protocol = prot
+        self.checksum = checksum
         self.src = src
         self.dest = dest
-        self.version = version
-        self.length = length
-        self.protocol = protocol
-        self.ttl = ttl
+        self.payload = payload
+
+
+    def setTime(self, time):
+        self.time = time
+
 
 class TCP:
-    def __init__(self, ip, src, dest, seq, ack, length):
-        self.ip = ip
-        self.src = src
-        self.dest = dest
+    def __init__(self, src_port, dest_port, seq, ack, offset, ecn, flags, window, checksum, urgentPointer, payload):
+        self.src_port = src_port
+        self.dest_port = dest_port
         self.sequence = seq
         self.ack = ack
-        self.length = length
+        self.offset = offset
+        self.ecn = ecn
+        self.flags = flags
+        self.window = window
+        self.checksum = checksum
+        self.urgentPointer = urgentPointer
+        self.payload = payload
+
+    def setIp(self, ip):
+        self.ip = ip
 
 class UDP:
-    def __init__(self, ip, src, dest, checksum, length):
-        self.ip = ip
-        self.src = src
-        self.dest = dest
-        self.checksum = checksum
+    def __init__(self, src_port, dest_port, length, checksum, payload):
+        self.src_port = src_port
+        self.dest_port = dest_port
         self.length = length
+        self.checksum = checksum
+        self.payload = payload
+
+    def setIp(self, ip):
+        self.ip = ip
 
 class HTTP:
-    def __init__(self, tcp, data):
-        self.tcp = tcp
+    def __init__(self, data):
         self.data = data
 
+    def setTcp(self, tcp):
+        self.tcp = tcp
+
 class DNS:
-    def __init__(self, transID, flags, numQuestions, numuAnswers, numAuthority, numAdditional):
+    def __init__(self, transID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, data):
         self.transID = transID
         self.flags = flags
         self.numQuestions = numQuestions
         self.numuAnswers = numuAnswers
         self.numAuthority = numAuthority
         self.numAdditional = numAdditional
+        self.data = data
+
+    def setUdp(self, udp):
+        self.udp = udp
+
+    def setType(self, type):
+        self.type = type
 
 def main():
     if platform.system() == "Windows":
@@ -59,95 +92,114 @@ def main():
     currentTime = datetime.datetime.now()
     endTime = currentTime + datetime.timedelta(seconds=int(timeout))
 
+    outFile = open("data.dump", "wb")
+
     while datetime.datetime.now() < endTime:
         raw = sniffer.recvfrom(65565)[0]
-        IP_version, IP_IHL, IP_service, IP_length, IP_id, IP_flags, IP_offset, IP_ttl, IP_prot, IP_checksum, IP_src, IP_dest, IP_payload = parseIP(raw)
+        ip = parseIP(raw)
+        ip.setTime(datetime.datetime.now())
 
-        print('*************** IP PACKET SNIFFED ***************')
-        print('Time: ' + str(datetime.datetime.now()))
-        print('Version: ' + str(IP_version))
-        print('IHL: '+ str(IP_IHL))
-        print('TOS: ' + str(IP_service))
-        print('ID: ' + str(IP_id))
-        print('Flags: ' + str(IP_flags))
-        print('Offset: ' + str(IP_offset))
-        print('Total Length: ' + str(IP_length))
-        print('TTL: ' + str(IP_ttl))
-        print('Protocol: ' + str(IP_prot))
-        print('Checksum: ' + str(IP_checksum))
-        print('Source IP: ' + IP_src)
-        print('Destination IP: ' + IP_dest)
-        print('Payload: ' + str(binascii.hexlify(IP_payload)))
+        if ip.protocol == 'TCP':
+            # print('--------------- TCP PACKET SNIFFED ---------------')
+            tcp = parseTCP(ip.payload)
+            tcp.setIp(ip)
 
-        if IP_prot == 'TCP':
-            print('--------------- TCP PACKET SNIFFED ---------------')
-            TCP_src_port, TCP_dest_port, TCP_seq, TCP_ack, TCP_offset, TCP_ecn, TCP_flags, TCP_window, TCP_checksum, TCP_urgentPointer, TCP_payload = parseTCP(IP_payload)
+            # print('Source Port: ' + str(tcp.src_port))
+            # print('Destination Port: ' + str(tcp.dest_port))
+            # print('Sequence Number: ' + str(tcp.sequence))
+            # print('Acknowledgment Number: ' + str(tcp.ack))
+            # print('Offset: ' + str(tcp.offset))
+            # print('ECN: ' + str(tcp.ecn))
+            # print('Flags: ' + str(tcp.flags))
+            # print('Window:: ' + str(tcp.window))
+            # print('Checksum: ' + str(tcp.checksum))
+            # print('Urgent Pointer: ' + str(tcp.urgentPointer))
+            # print('Payload: ' + str(binascii.hexlify(tcp.payload)))
 
-            print('Source Port: ' + str(TCP_src_port))
-            print('Destination Port: ' + str(TCP_dest_port))
-            print('Sequence Number: ' + str(TCP_seq))
-            print('Acknowledgment Number: ' + str(TCP_ack))
-            print('Offset: ' + str(TCP_offset))
-            print('ECN: ' + str(TCP_ecn))
-            print('Flags: ' + str(TCP_flags))
-            print('Window:: ' + str(TCP_window))
-            print('Checksum: ' + str(TCP_checksum))
-            print('Urgent Pointer: ' + str(TCP_urgentPointer))
-            print('Payload: ' + str(binascii.hexlify(TCP_payload)))
+            if tcp.src_port == 80 or tcp.dest_port == 80:
+                http = parseHTTP(tcp.payload)
+                http.setTcp(tcp)
 
-            if TCP_src_port == 80 or TCP_dest_port == 80:
-                HTTP_Data = parseHTTP(TCP_payload)
-                if len(HTTP_Data) > 0:
-                    print('~~~~~~~~~~~~~~ HTTP PACKET SNIFFED ~~~~~~~~~~~~~~')
-                    print(HTTP_Data)
-                    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                # if len(http.data) > 0:
+                #     print('~~~~~~~~~~~~~~ HTTP PACKET SNIFFED ~~~~~~~~~~~~~~')
+                #     print(http.data)
+                #     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-            if TCP_src_port == 443 or TCP_dest_port == 443:
-                HTTPS_Data = parseHTTP(TCP_payload)
-                if len(HTTPS_Data) > 0:
-                    print('~~~~~~~~~~~~~~ HTTPS PACKET SNIFFED ~~~~~~~~~~~~~')
-                    print(HTTPS_Data)
-                    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                pickle.dump(http, outFile, protocol=pickle.HIGHEST_PROTOCOL)
+            elif tcp.src_port == 443 or tcp.dest_port == 443:
+                https = parseHTTP(tcp.payload)
+                https.setTcp(tcp)
 
-            print('-------------------------------------------------')
+                # if len(https.data) > 0:
+                #     print('~~~~~~~~~~~~~~ HTTPS PACKET SNIFFED ~~~~~~~~~~~~~')
+                #     print(https.data)
+                #     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-        if IP_prot == 'UDP':
-            print('--------------- UDP PACKET SNIFFED ---------------')
-            UDP_src_port, UDP_dest_port, UDP_length, UDP_checksum, UDP_payload = parseUDP(IP_payload)
-            print('Source Port: ' + str(UDP_src_port))
-            print('Destination Port: ' + str(UDP_dest_port))
-            print('Length: ' + str(UDP_length))
-            print('Checksum: ' + str(UDP_checksum))
-            print('Payload: ' + str(binascii.hexlify(UDP_payload)))
+                pickle.dump(https, outFile, protocol=pickle.HIGHEST_PROTOCOL)
+            else:
+                pickle.dump(tcp, outFile, protocol=pickle.HIGHEST_PROTOCOL)
 
-            if UDP_dest_port == 53:
-                TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, querySection = parseDNSQuery(UDP_payload)
-                print('~~~~~~~~~~~~~~ DNS QUERY SNIFFED ~~~~~~~~~~~~~~')
-                print('TransID: '+str(TransID))
-                print('Flags: 0x' + format(flags, '02x'))
-                print('NumQuestions: ' + str(numQuestions))
-                print('NumuAnswers: ' + str(numuAnswers))
-                print('NumAuthority: ' + str(numAuthority))
-                print('NumAdditional: ' + str(numAdditional))
-                print('QuerySection: ' + querySection)
-                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            # print('-------------------------------------------------')
+        elif ip.protocol == 'UDP':
+            # print('--------------- UDP PACKET SNIFFED ---------------')
+            udp = parseUDP(ip.payload)
+            udp.setIp(ip)
 
-            if UDP_src_port == 53:
-                TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, respSection = parseDNSResp(UDP_payload)
-                print('~~~~~~~~~~~~~~~ DNS RESP SNIFFED ~~~~~~~~~~~~~~~')
-                print('TransID: '+str(TransID))
-                print('Flags: 0x' + format(flags, '02x'))
-                print('NumQuestions: ' + str(numQuestions))
-                print('NumuAnswers: ' + str(numuAnswers))
-                print('NumAuthority: ' + str(numAuthority))
-                print('NumAdditional: ' + str(numAdditional))
-                print('RespSection: ' + respSection)
-                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            # print('Source Port: ' + str(udp.src_port))
+            # print('Destination Port: ' + str(udp.dest_port))
+            # print('Length: ' + str(udp.length))
+            # print('Checksum: ' + str(udp.checksum))
+            # print('Payload: ' + str(binascii.hexlify(udp.payload)))
+
+            if udp.dest_port == 53:
+                dns = parseDNSQuery(udp.payload)
+                # print('~~~~~~~~~~~~~~ DNS QUERY SNIFFED ~~~~~~~~~~~~~~')
+                # print('TransID: '+str(dns.transID))
+                # print('Flags: 0x' + format(dns.flags, '02x'))
+                # print('NumQuestions: ' + str(dns.numQuestions))
+                # print('NumuAnswers: ' + str(dns.numuAnswers))
+                # print('NumAuthority: ' + str(dns.numAuthority))
+                # print('NumAdditional: ' + str(dns.numAdditional))
+                # print('QuerySection: ' + dns.data)
+                # print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                pickle.dump(dns, outFile, protocol=pickle.HIGHEST_PROTOCOL)
+            elif udp.src_port == 53:
+                dns = parseDNSResp(udp.payload)
+                # print('~~~~~~~~~~~~~~~ DNS RESP SNIFFED ~~~~~~~~~~~~~~~')
+                # print('TransID: '+str(dns.transID))
+                # print('Flags: 0x' + format(dns.flags, '02x'))
+                # print('NumQuestions: ' + str(dns.numQuestions))
+                # print('NumuAnswers: ' + str(dns.numuAnswers))
+                # print('NumAuthority: ' + str(dns.numAuthority))
+                # print('NumAdditional: ' + str(dns.numAdditional))
+                # print('RespSection: ' + dns.data)
+                # print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                pickle.dump(dns, outFile, protocol=pickle.HIGHEST_PROTOCOL)
+            else:
+                pickle.dump(udp, outFile, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-            print('-------------------------------------------------')
+            # print('-------------------------------------------------')
+        else:
+            pickle.dump(ip, outFile, protocol=pickle.HIGHEST_PROTOCOL)
 
-        print('*************************************************\n\n')
+        # print('*************************************************\n\n')
+
+    outFile.close()
+
+    inFile = open("data.dump","rb")
+
+    while True:
+        try:
+            packet = pickle.load(inFile)
+            print(repr(packet))
+        except (EOFError):
+            print("done")
+            break
+
+
+    print("hello")
+    inFile.close()
 
 
 def parseIP(raw):
@@ -184,7 +236,8 @@ def parseIP(raw):
     if prot in P:
         prot = P[prot]
 
-    return version, IHL, service, length, id, flags, offset, ttl, prot, checksum, src, dest, payload
+
+    return IP(version, IHL, service, length, id, flags, offset, ttl, prot, checksum, src, dest, payload)
 
 def parseTCP(ip_payload):
     src_port, dest_port, seq, ack, offset_resv_ecn_control, window, checksum, urgentPointer = struct.unpack('!HHLLHHHH', ip_payload[:20])
@@ -197,32 +250,36 @@ def parseTCP(ip_payload):
     flags = ('Urgent, ' if (((control >> 5) & 1) == 1) else '') + ('Acknowledgment, ' if (((control >> 4) & 1) == 1) else '') + ('Push, ' if (((control >> 3) & 1) == 1) else '') + ('Reset, ' if (((control >> 2) & 1) == 1) else '') + ('Synchronize, ' if (((control >> 1) & 1) == 1) else '') + ('Finished' if ((control & 1) == 1) else '')
 
     payload = ip_payload[(offset*4):]
-    return src_port, dest_port, seq, ack, offset, ecn, flags, window, checksum, urgentPointer, payload
+
+    return TCP(src_port, dest_port, seq, ack, offset, ecn, flags, window, checksum, urgentPointer, payload)
 
 def parseUDP(ip_payload):
     src_port, dest_port, length, checksum = struct.unpack('!HHHH', ip_payload[:8])
     payload = ip_payload[8:]
 
-    return src_port, dest_port, length, checksum, payload
+    return UDP(src_port, dest_port, length, checksum, payload)
 
 def parseHTTP(tcp_payload):
     http_data = ''
     try:
-        http_data = tcp_payload.decode('utf-8', 'ignore')
+        http_data = tcp_payload
     except:
         http_data = str(tcp_payload)
 
-    return http_data
+    return HTTP(http_data)
 
 def parseDNSQuery(udp_payload):
     TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional   = struct.unpack('!HHHHHH', udp_payload[:12])
     querySection = udp_payload[12:]
+
     try:
         querySection = querySection.decode('ascii', 'ignore')
     except:
         querySection = str(querySection)
 
-    return TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, querySection
+    dnsPacket = DNS(TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, querySection)
+    dnsPacket.setType(flags >> 15)
+    return dnsPacket
 
 def parseDNSResp(udp_payload):
     TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional   = struct.unpack('!HHHHHH', udp_payload[:12])
@@ -232,6 +289,8 @@ def parseDNSResp(udp_payload):
     except:
         respSection = str(respSection)
 
-    return TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, respSection
+    dnsPacket = DNS(TransID, flags, numQuestions, numuAnswers, numAuthority, numAdditional, respSection)
+    dnsPacket.setType(flags >> 15)
+    return dnsPacket
 
 main()
